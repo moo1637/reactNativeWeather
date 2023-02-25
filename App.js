0cf21f05a05e724e43f3ec4261faf846475320de
moo1637 <moo1637@gmail.com>
2023-02-25 17:03:41 +0900

    finish

--- App.js
+++ App.js
@@ -1,34 +1,64 @@
 import React, { useEffect, useState } from "react";
-import { Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
-
+import {
+  ActivityIndicator,
+  Dimensions,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import Weather from "./DailyWeather";
 import * as Location from "expo-location";
+import { Fontisto } from "@expo/vector-icons";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
+const API_KEY = "blabla..."; // open api 사용 대체
+
+const icons = {
+  Clear: "day-sunny",
+  Clouds: "cloudy",
+  Rain: "rain",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snow",
+  Drizzle: "day-rain",
+  Thunderstorm: "lightning",
+};
+
 export default function App() {
-  const [location, setLocation] = useState();
+  const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
-  const ask = async () => {
+
+  const getWeather = async () => {
+    // 위치 권한 요청
     const { granted } = await Location.requestForegroundPermissionsAsync();
     if (!granted) {
       setOk(false);
     }
+    // 현재 위치 좌표 get
     const {
       coords: { latitude, longitude },
     } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    // 위치 좌표 주소 변환
     const location = await Location.reverseGeocodeAsync(
       { latitude, longitude },
       { useGoogleMaps: false }
     );
-    console.log(location);
+    setCity(location[0].city);
+
+    // fetch('https://blabla...')
+    setDays(Weather.daily);
   };
   useEffect(() => {
-    ask();
-  });
+    getWeather();
+  }, []);
   return (
     <View style={styles.container}>
+      <StatusBar style="light" />
       <View style={styles.city}>
-        <Text style={styles.cityName}>서울</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView
         horizontal
@@ -36,54 +66,79 @@ export default function App() {
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.weather}
       >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
+        {days.length === 0 ? (
+          <View style={{ ...styles.day, alignItems: "center" }}>
+            <ActivityIndicator
+              color="white"
+              size="large"
+              style={{ marginTop: 10 }}
+            />
+          </View>
+        ) : (
+          days.map((day, i) => (
+            <View key={i} style={styles.day}>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  width: "100%",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Text style={styles.temp}>
+                  {parseFloat(day.temp.day).toFixed(1)}
+                </Text>
+                <Fontisto
+                  name={icons[day.weather[0].main]}
+                  size={68}
+                  color="white"
+                />
+              </View>
+
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+            </View>
+          ))
+        )}
       </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: "tomato" },
+  container: { flex: 1, backgroundColor: "skyblue" },
   city: {
     flex: 1,
-    // backgroundColor: "blue",
     justifyContent: "center",
     alignItems: "center",
   },
   cityName: {
-    fontSize: 68,
+    fontSize: 70,
     fontWeight: "500",
+    color: "white",
   },
   weather: {},
   day: {
     width: SCREEN_WIDTH,
-    alignItems: "center",
+    alignItems: "flex-start",
+    paddingHorizontal: 20,
   },
   temp: {
     marginTop: 50,
-    fontSize: 178,
+    fontSize: 100,
+    fontWeight: "700",
+    color: "white",
   },
   description: {
-    marginTop: -30,
-    fontSize: 60,
+    marginTop: -20,
+    fontSize: 30,
+    fontWeight: "600",
+    color: "white",
+  },
+  tinyText: {
+    marginTop: -10,
+    fontSize: 20,
+    fontWeight: "500",
+    color: "white",
   },
 });
---
+++ DailyWeather.js
@@ -0,0 +1,130 @@
+export default {
+  daily: [
+    {
+      weather: [
+        {
+          id: 500,
+          main: "Rain",
+          description: "light rain",
+          icon: "10d",
+        },
+      ],
+      temp: {
+        day: 27.79,
+        min: 27.09,
+        max: 28.07,
+        night: 27.09,
+        eve: 27.21,
+        morn: 27.49,
+      },
+    },
+    {
+      weather: [
+        {
+          id: 501,
+          main: "Clouds",
+          description: "few clouds",
+          icon: "10d",
+        },
+      ],
+      temp: {
+        day: 27.79,
+        min: 27.09,
+        max: 28.07,
+        night: 27.09,
+        eve: 27.21,
+        morn: 27.49,
+      },
+    },
+    {
+      weather: [
+        {
+          id: 502,
+          main: "Clear",
+          description: "clear sky",
+          icon: "10d",
+        },
+      ],
+      temp: {
+        day: 27.79,
+        min: 27.09,
+        max: 28.07,
+        night: 27.09,
+        eve: 27.21,
+        morn: 27.49,
+      },
+    },
+    {
+      weather: [
+        {
+          id: 503,
+          main: "Clear",
+          description: "clear sky",
+          icon: "10d",
+        },
+      ],
+      temp: {
+        day: 27.79,
+        min: 27.09,
+        max: 28.07,
+        night: 27.09,
+        eve: 27.21,
+        morn: 27.49,
+      },
+    },
+    {
+      weather: [
+        {
+          id: 504,
+          main: "Clear",
+          description: "clear sky",
+          icon: "10d",
+        },
+      ],
+      temp: {
+        day: 27.79,
+        min: 27.09,
+        max: 28.07,
+        night: 27.09,
+        eve: 27.21,
+        morn: 27.49,
+      },
+    },
+    {
+      weather: [
+        {
+          id: 505,
+          main: "Clear",
+          description: "clear sky",
+          icon: "10d",
+        },
+      ],
+      temp: {
+        day: 27.79,
+        min: 27.09,
+        max: 28.07,
+        night: 27.09,
+        eve: 27.21,
+        morn: 27.49,
+      },
+    },
+    {
+      weather: [
+        {
+          id: 506,
+          main: "Rain",
+          description: "heavy rain",
+          icon: "10d",
+        },
+      ],
+      temp: {
+        day: 27.79,
+        min: 27.09,
+        max: 28.07,
+        night: 27.09,
+        eve: 27.21,
+        morn: 27.49,
+      },
+    },
+  ],
+};
